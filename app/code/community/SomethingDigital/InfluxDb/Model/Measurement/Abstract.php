aae8a1df0b85d752155341c2b280a75258673e93
Max Chadwick <mpchadwick@gmail.com>
2016-06-27 13:29:59 -0400

    Explicitly add api property

--- app/code/community/SomethingDigital/InfluxDb/Model/Measurement/Abstract.php
+++ app/code/community/SomethingDigital/InfluxDb/Model/Measurement/Abstract.php
@@ -2,6 +2,9 @@
 
 abstract class SomethingDigital_InfluxDb_Model_Measurement_Abstract
 {
+    /** @var SomethingDigital_InfluxDb_Model_Api */
+    protected $api;
+
     public function __construct()
     {
         $this->api = Mage::getModel('sd_influxdb/api');
